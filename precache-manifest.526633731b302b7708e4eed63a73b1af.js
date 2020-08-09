bbcb718e20e5b467a9a7a1d22431696896c2f4bf
Miikka Lehtinen <miikka.lehtinen@hotmail.com>
2020-08-09 21:08:34 +0300

    Updates

---
+++ precache-manifest.526633731b302b7708e4eed63a73b1af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8930d227436825f7dd0f855d7e0cfb1",
+    "url": "/Personal-Trainer-Task/index.html"
+  },
+  {
+    "revision": "9f7ca49903b941249b8b",
+    "url": "/Personal-Trainer-Task/static/css/2.831ac3c2.chunk.css"
+  },
+  {
+    "revision": "392939d16b26a35dd66f",
+    "url": "/Personal-Trainer-Task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9f7ca49903b941249b8b",
+    "url": "/Personal-Trainer-Task/static/js/2.e6f238e6.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Personal-Trainer-Task/static/js/2.e6f238e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392939d16b26a35dd66f",
+    "url": "/Personal-Trainer-Task/static/js/main.38973485.chunk.js"
+  },
+  {
+    "revision": "51a70d6f698304a1d6d0",
+    "url": "/Personal-Trainer-Task/static/js/runtime-main.26aff2fa.js"
+  }
+]);
